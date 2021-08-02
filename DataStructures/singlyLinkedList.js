c0318dfa7ed850d76c30e471fd4084f797bd940c
Jim Carter <jameslesliecarter@gmail.com>
2021-08-02 13:10:57 -0400

    sll get/set

--- DataStructures/singlyLinkedList.js
+++ DataStructures/singlyLinkedList.js
@@ -53,4 +53,39 @@ class singlyLinkedList {
     this.length --;
     return temp;
   }
+
+  unshift(val) {
+    let newHead = new Node(val);
+    if (!this.head) {
+      this.head = newHead;
+      this.tail = newHead;
+    } else {
+      newHead.next = this.head;
+      this.head = newHead;
+    }
+    this.length ++;
+    return this;
+  }
+
+  get(index) {
+    if (index > this.length || index < 0) {
+      return null;
+    }
+    let count = 0;
+    let target = this.head;
+    while (count !== index) {
+      target = target.next;
+      count ++;
+    }
+    return target;
+  }
+
+  set(index, val) {
+    let target = this.get(val);
+    if (target) {
+      target.val = val;
+      return true;
+    }
+    return false;
+  }
 }
