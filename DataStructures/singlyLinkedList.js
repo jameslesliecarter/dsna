d20f9859cad374ce54168205aab82262197f69ee
Jim Carter <jameslesliecarter@gmail.com>
2021-08-02 12:23:43 -0400

    linked list push

---
+++ DataStructures/singlyLinkedList.js
@@ -0,0 +1,27 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class singlyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length ++;
+    return this;
+  }
+}
