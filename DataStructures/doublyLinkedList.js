e5894623a9b92a910403e122afb86ead12aed18f
Jim Carter <jameslesliecarter@gmail.com>
2021-08-16 12:09:53 -0400

    dll push

--- DataStructures/doublyLinkedList.js
+++ DataStructures/doublyLinkedList.js
@@ -5,3 +5,25 @@ class Node {
     this.next = null;
   }
 }
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.length ++;
+    return this;
+  }
+}
