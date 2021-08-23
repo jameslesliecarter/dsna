9c91e4c3c9f38695c482bae685fe806dd520b6f5
Jim Carter <jameslesliecarter@gmail.com>
2021-08-23 10:35:30 -0400

    BST pre order DFS list and val search

--- DataStructures/binarySearchTree.js
+++ DataStructures/binarySearchTree.js
@@ -38,4 +38,82 @@ class BinarySearchTree {
       }
     }
   }
+
+  find(val) {
+    if (!this.root) {
+      return null;
+    }
+    let current = this.root;
+    while (true) {
+      if (current.val === val) {
+        return current;
+      } else if (val > current.val) {
+        if (current.right) {
+          current = current.right;
+        } else {
+          return null;
+        }
+      } else {
+        if (current.left) {
+          current = current.left;
+        } else {
+          return null;
+        }
+      }
+    }
+  }
+
+  breadthFirstSearch(val) {
+    if (!this.root) {
+      return null;
+    }
+    let queue = [];
+    let current;
+    queue.push(this.root);
+    while (queue.length) {
+      current = queue.shift();
+      if (current.val === val) {
+        return current;
+      }
+      if (current.left) {
+        queue.push(current.left);
+      }
+      if (current.right) {
+        queue.push(current.right);
+      }
+    }
+    return null;
+  }
+
+  preOrderDFS() {
+    let vals = [];
+    let scan = function(node) {
+      vals.push(node.val);
+      if (node.left) {
+        scan(node.left);
+      }
+      if (node.right) {
+        scan(node.right);
+      }
+    }
+    scan(this.root);
+    return vals;
+  }
+
+  preOrderDFS2(val) {
+    let scan = function(node) {
+      if (node.val === val) {
+        return node;
+      }
+      if (node.left) {
+        scan(node.left);
+      }
+      if (node.right) {
+        scan(node.right);
+      }
+    }
+    let result = scan(this.root);
+    return result ? result : null;
+  }
+
 }
