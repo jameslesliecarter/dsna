643be161035b66fcf8fc7403c683bbe75de5a1e1
Jim Carter <jameslesliecarter@gmail.com>
2021-08-25 17:33:29 -0400

    remove edge and remove vertex

--- DataStructures/graph.js
+++ DataStructures/graph.js
@@ -8,4 +8,33 @@ class Graph {
       this.adjacencyList[val] = [];
     }
   }
+
+  removeVertex(val) {
+    delete this.adjacencyList[val];
+    for (let key in this.adjacencyList) {
+      if this.adjacencyList[key].contains(val) {
+        this.adjacencyList[key].splice(this.adjacencyList[key].indexOf(val), this.adjacencyList[key].indexOf(val) + 1);
+      }
+    }
+  }
+
+  addEdge(vOne, vTwo) {
+    if (!Array.isArray(this.adjacencyList[vOne]) ||
+        !Array.isArray(this.adjacencyList[vTwo])) {
+      return;
+    }
+    this.adjacencyList[vOne].push(vTwo);
+    this.adjacencyList[vTwo].push(vOne);
+  }
+
+  removeEdge(vOne, vTwo) {
+    if (!Array.isArray(this.adjacencyList[vOne]) ||
+        !Array.isArray(this.adjacencyList[vTwo])) {
+      return;
+    }
+    this.adjacencyList[vOne].splice(this.adjacencyList[vOne].indexOf(vTwo), this.adjacencyList[vOne].indexOf(vTwo) + 1);
+    this.adjacencyList[vTwo].splice(this.adjacencyList[vTwo].indexOf(vOne), this.adjacencyList[vTwo].indexOf(vOne) + 1);
+
+  }
+
 }
