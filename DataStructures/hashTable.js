eb830e378bb8dadb0a4e2681cc24e13da942a493
Jim Carter <jameslesliecarter@gmail.com>
2021-08-25 12:53:56 -0400

    getting and setting hash table

--- DataStructures/hashTable.js
+++ DataStructures/hashTable.js
@@ -1,7 +1,37 @@
-const hash = require('./hash.js');
-
 class HashTable {
-  constructor() {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+  _hash(key) {
+    let total = 0;
+    const WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i ++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length
+    }
+    return total;
+  }
 
+  set(key, val) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [[key, val]];
+    } else {
+      this.keyMap[index].push([key, val]);
+    }
+  }
+
+  get(key) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      return undefined;
+    }
+    for (let i = 0; i < this.keyMap[index].length; i ++) {
+      if (this.keyMap[index][i][0] === key) {
+        return this.keyMap[index][i][1];
+      }
+    }
+    return undefined;
   }
 }
